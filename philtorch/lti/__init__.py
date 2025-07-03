c31ebb3f147f31ae43c830ba740b93a43189a80b
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2025-07-03 17:45:46 +0100

    fix: update __all__ to include diag_state_space and state_space

--- philtorch/lti/__init__.py
+++ philtorch/lti/__init__.py
@@ -1,4 +1,4 @@
 from .filters import lfilter
-from .ssm import state_space_recursion
+from .ssm import state_space_recursion, diag_state_space, state_space
 
-__all__ = ["lfilter", "state_space_recursion"]
+__all__ = ["lfilter", "state_space_recursion", "diag_state_space", "state_space"]
