7a342579ae470a1f0abf56eb5fc413d28b88ee21
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2025-07-04 10:54:04 +0100

    feat: add fir to __all__ exports in lti module

--- philtorch/lti/__init__.py
+++ philtorch/lti/__init__.py
@@ -1,4 +1,4 @@
-from .filters import lfilter
+from .filters import lfilter, fir
 from .ssm import state_space_recursion, diag_state_space, state_space
 
-__all__ = ["lfilter", "state_space_recursion", "diag_state_space", "state_space"]
+__all__ = ["lfilter", "state_space_recursion", "diag_state_space", "state_space", "fir"]
