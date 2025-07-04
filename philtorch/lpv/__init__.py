4cd789f505f04b0a7083dab905b8464dac91c3ae
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2025-07-04 20:17:01 +0100

    feat: add scan to __all__ in lpv and lti modules

--- philtorch/lpv/__init__.py
+++ philtorch/lpv/__init__.py
@@ -1,4 +1,4 @@
 from .filters import lfilter
+from .scan import scan
 
-
-__all__ = ["lfilter"]
+__all__ = ["lfilter", "scan"]
--- philtorch/lti/__init__.py
+++ philtorch/lti/__init__.py
@@ -1,4 +1,12 @@
 from .filters import lfilter, fir
 from .ssm import state_space_recursion, diag_state_space, state_space
+from .scan import scan
 
-__all__ = ["lfilter", "state_space_recursion", "diag_state_space", "state_space", "fir"]
+__all__ = [
+    "lfilter",
+    "state_space_recursion",
+    "diag_state_space",
+    "state_space",
+    "fir",
+    "scan",
+]
