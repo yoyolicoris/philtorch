a8e479942183459805cbc745b8528768702ab4e8
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2025-07-24 11:36:01 +0100

    update recur2.cu

--- philtorch/csrc/recur2.cu
+++ philtorch/csrc/recur2.cu
@@ -1,3 +1,4 @@
+#include <Python.h>
 #include <assert.h>
 #include <c10/cuda/CUDAException.h>
 #include <c10/cuda/CUDAGuard.h>
@@ -6,17 +7,29 @@
 #include <thrust/device_vector.h>
 #include <thrust/execution_policy.h>
 #include <thrust/functional.h>
+#include <thrust/iterator/transform_output_iterator.h>
 #include <thrust/pair.h>
 #include <thrust/scan.h>
 #include <thrust/transform.h>
 #include <torch/script.h>
 #include <torch/torch.h>
 
-// template <typename T>
-// struct sqm2_pair {
-//     T mat_entries[4];
-//     T vec_entries[2];
-// };
+extern "C" {
+/* Creates a dummy empty _C module that can be imported from Python.
+   The import from Python will load the .so associated with this extension
+   built from this file, so that all the TORCH_LIBRARY calls below are run.*/
+PyObject *PyInit__C(void) {
+    static struct PyModuleDef module_def = {
+        PyModuleDef_HEAD_INIT,
+        "_C", /* name of module */
+        NULL, /* module documentation, may be NULL */
+        -1,   /* size of per-interpreter state of the module,
+                 or -1 if the module keeps state in global variables. */
+        NULL, /* methods */
+    };
+    return PyModule_Create(&module_def);
+}
+}
 
 template <typename T>
 using sqm2_pair = cuda::std::tuple<T, T, T, T, T, T>;
@@ -41,35 +54,124 @@ struct recur2_binary_op {
 
         return cuda::std::make_tuple(
             b_a * a_a + b_b * a_c, b_a * a_b + b_b * a_d, b_c * a_a + b_d * a_c,
-            b_c * a_b + b_d * a_d, b_a * a_a_vec + b_b * a_b_vec + a_a_vec,
+            b_c * a_b + b_d * a_d, b_a * a_a_vec + b_b * a_b_vec + b_a_vec,
             b_c * a_a_vec + b_d * a_b_vec + b_b_vec);
     }
 };
 
+template <typename T>
+struct output_unary_op {
+    __host__ __device__ cuda::std::tuple<T, T> operator()(
+        const sqm2_pair<T> &state) const {
+        return thrust::make_tuple(thrust::get<4>(state), thrust::get<5>(state));
+    }
+};
+
+template <typename T>
+struct share_A_input_op {
+    const T *A;
+    int n_steps;
+    __host__ __device__ sqm2_pair<T> operator()(int i, const T &x_0,
+                                                const T &x_1) const {
+        int idx = i % n_steps;
+        return thrust::make_tuple(A[idx], A[idx + n_steps],
+                                  A[idx + n_steps * 2], A[idx + n_steps * 3],
+                                  x_0, x_1);
+    }
+};
+
 template <typename scalar_t>
-void matrix_recurrence_second_order(const scalar_t *A, const scalar_t *x,
-                                    scalar_t *out, int n_steps) {
-    thrust::device_vector<sqm2_pair<scalar_t>> pairs(n_steps);
+void batch_mat_recur_second_order(const scalar_t *A, const scalar_t *x,
+                                  scalar_t *out, int n_steps) {
+    thrust::inclusive_scan(
+        thrust::device,
+        thrust::make_zip_iterator(A, A + n_steps, A + n_steps * 2,
+                                  A + n_steps * 3, x, x + n_steps),
+        thrust::make_zip_iterator(A + n_steps, A + n_steps * 2, A + n_steps * 3,
+                                  A + n_steps * 4, x + n_steps,
+                                  x + n_steps * 2),
+        thrust::make_transform_output_iterator(
+            thrust::make_zip_iterator(out, out + n_steps),
+            output_unary_op<scalar_t>()),
+        recur2_binary_op<scalar_t>());
+}
 
-    thrust::counting_iterator<int> A_iter(0);
-    // Initialize input_states and output_states
-    thrust::copy(thrust::device,
-                 thrust::make_zip_iterator(A, A + n_steps, A + n_steps * 2,
-                                           A + n_steps * 3, x, x + n_steps),
-                 thrust::make_zip_iterator(A + n_steps, A + n_steps * 2,
-                                           A + n_steps * 3, A + n_steps * 4,
-                                           x + n_steps, x + n_steps * 2),
-                 pairs.begin());
+template <typename scalar_t>
+void share_mat_recur_second_order(const scalar_t *A, const scalar_t *x,
+                                  scalar_t *out, int n_steps, int n_batches) {
+    auto total_steps = n_steps * n_batches;
+    thrust::counting_iterator<int> iter(0);
+    auto share_input_op =
+        thrust::make_zip_function(share_A_input_op<scalar_t>{A, n_steps});
+    thrust::inclusive_scan(
+        thrust::device,
+        thrust::make_transform_iterator(
+            thrust::make_zip_iterator(iter, x, x + total_steps),
+            share_input_op),
+        thrust::make_transform_iterator(
+            thrust::make_zip_iterator(iter + total_steps, x + total_steps,
+                                      x + total_steps * 2),
+            share_input_op),
+        thrust::make_transform_output_iterator(
+            thrust::make_zip_iterator(out, out + total_steps),
+            output_unary_op<scalar_t>()),
+        recur2_binary_op<scalar_t>());
+}
 
-    recur2_binary_op<scalar_t> binary_op;
-    thrust::inclusive_scan(thrust::device, pairs.begin(), pairs.end(),
-                           pairs.begin(), binary_op);
+at::Tensor mat_recur_second_order_wrapper(const at::Tensor &A,
+                                          const at::Tensor &zi,
+                                          const at::Tensor &x) {
+    TORCH_CHECK(x.is_floating_point() || x.is_complex(),
+                "Input must be floating point or complex");
+    TORCH_CHECK(zi.scalar_type() == zi.scalar_type(),
+                "zi must have the same scalar type as input");
+    TORCH_CHECK(A.scalar_type() == A.scalar_type(),
+                "A must have the same scalar type as input");
+    TORCH_CHECK(A.dim() == 3 || A.dim() == 4, "A must be a 3D or 4D tensor");
+    TORCH_CHECK(x.size(2) == 2, "Input x must have a last dimension of size 2");
+    TORCH_CHECK(A.size(-1) == 2 && A.size(-2) == 2,
+                "Last two dimensions of A must be of size 2");
 
-    auto take_last2 = [](const sqm2_pair<scalar_t> &p) {
-        return thrust::make_tuple(thrust::get<4>(p), thrust::get<5>(p));
-    };
+    auto n_steps = x.size(1) + 1;  // +1 for the initial state
+    auto n_batches = x.size(0);
 
-    thrust::transform(thrust::device, pairs.begin(), pairs.end(),
-                      thrust::make_zip_iterator(out, out + n_steps),
-                      take_last2);
+    auto A_contiguous =
+        at::pad(A, {0, 0, 0, 0, 1, 0}).view({-1, 4}).t().contiguous();
+    auto x_contiguous =
+        at::cat({zi.unsqueeze(1), x}, 1).view({-1, 2}).t().contiguous();
+    auto out = at::empty_like(x_contiguous);
+
+    const at::cuda::OptionalCUDAGuard device_guard(device_of(x));
+
+    if (A.dim() == 4) {
+        // Batch
+        AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES(
+            x.scalar_type(), "batch_mat_recur_second_order", [&] {
+                batch_mat_recur_second_order<scalar_t>(
+                    A_contiguous.const_data_ptr<scalar_t>(),
+                    x_contiguous.const_data_ptr<scalar_t>(),
+                    out.mutable_data_ptr<scalar_t>(), n_steps * n_batches);
+            });
+    } else {
+        // Shared
+        AT_DISPATCH_FLOATING_AND_COMPLEX_TYPES(
+            x.scalar_type(), "share_mat_recur_second_order", [&] {
+                share_mat_recur_second_order<scalar_t>(
+                    A_contiguous.const_data_ptr<scalar_t>(),
+                    x_contiguous.const_data_ptr<scalar_t>(),
+                    out.mutable_data_ptr<scalar_t>(), n_steps, n_batches);
+            });
+    }
+    return out.t()
+        .reshape({n_batches, n_steps, 2})
+        .slice(1, 1, n_steps)
+        .contiguous();  // Remove the initial state from the output
+}
+
+TORCH_LIBRARY(philtorch, m) {
+    m.def("philtorch::recur2(Tensor A, Tensor zi, Tensor x) -> Tensor");
+}
+
+TORCH_LIBRARY_IMPL(philtorch, CUDA, m) {
+    m.impl("recur2", &mat_recur_second_order_wrapper);
 }
