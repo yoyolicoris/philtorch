ab36a1786f29103244b50e41eb2166c9478a2e3f
josephDSPLab <josephDSPLab@gmail.com>
2025-08-16 00:37:28 +0800

    let the branch name PEP 440 compatible

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 import os
 import glob
 import torch
+import re
 from torch.utils.cpp_extension import (
     CppExtension,
     CUDAExtension,
@@ -10,6 +11,23 @@ from torch.utils.cpp_extension import (
 )
 
 library_name = "philtorch"
+
+
+def format_branch_name(name):
+    # "(fix|feat)/issue-name"
+    pattern = re.compile("^(fix|feat)\/(?P<branch>.+)")
+
+    match = pattern.search(name)
+    if match:
+        return f"dev+{match.group(0)}" # => dev+"(fix|feat)/issue-name"
+
+    # function is called even if branch name is not used in a current template
+    # just left properly named branches intact
+    if name in ["master", "dev"]:
+        return name
+
+    # fail in case of wrong branch names like "bugfix/issue-unknown"
+    raise ValueError(f"Wrong branch name: {name}")
 
 
 def get_extensions():
