954b9b9bbdf9a05cb2ee1b580fa76695d6843e5e
YCY <chin-yun.yu@qmul.ac.uk>
2025-09-23 12:19:14 +0200

    feat: LTI Recurrence Autograd (#7)
    * feat: implement LTI recurrence with forward, backward, and JVP methods
    
    * refactor: rename Recurrence class to LTIRecurrence for clarity
    
    * fix: improve gradient calculation in LTIRecurrence by ensuring proper concatenation of gradients
    
    * feat: add first order LTI recurrence tests with gradient checks
    
    * feat: add LTIRecurrence extension backend into state_space

--- tests/test_grad.py
+++ tests/test_grad.py
@@ -3,6 +3,7 @@ import torch
 from torch.autograd.gradcheck import gradcheck, gradgradcheck
 from philtorch.lpv.ssm import MatrixRecurrence
 from philtorch.lti.ssm import LTIMatrixRecurrence
+from philtorch.lti.recur import LTIRecurrence
 
 
 @pytest.mark.parametrize(
@@ -155,3 +156,75 @@ def test_second_order_lti(
 
     assert gradcheck(LTIMatrixRecurrence.apply, (A, zi, x), check_forward_ad=True)
     assert gradgradcheck(LTIMatrixRecurrence.apply, (A, zi, x), check_fwd_over_rev=True)
+
+
+@pytest.mark.parametrize(
+    "x_requires_grad",
+    [True],
+)
+@pytest.mark.parametrize(
+    "a_requires_grad",
+    [True, False],
+)
+@pytest.mark.parametrize(
+    "zi_requires_grad",
+    [True, False],
+)
+@pytest.mark.parametrize(
+    "samples",
+    [11],
+)
+@pytest.mark.parametrize(
+    "cmplx",
+    [True, False],
+)
+@pytest.mark.parametrize(
+    "device",
+    [
+        "cpu",
+        pytest.param(
+            "cuda",
+            marks=pytest.mark.skipif(
+                not torch.cuda.is_available(), reason="CUDA not available"
+            ),
+        ),
+    ],
+)
+@pytest.mark.parametrize(
+    "share_a",
+    [True, False],
+)
+def test_first_order_lti(
+    x_requires_grad: bool,
+    a_requires_grad: bool,
+    zi_requires_grad: bool,
+    samples: int,
+    cmplx: bool,
+    device: str,
+    share_a: bool,
+):
+    batch_size = 3
+    x, a, zi = tuple(
+        x.to(device)
+        for x in [
+            torch.randn(
+                batch_size,
+                samples,
+                dtype=torch.double if not cmplx else torch.complex128,
+            ),
+            torch.randn(
+                *((batch_size,) if not share_a else (1,)),
+                dtype=torch.double if not cmplx else torch.complex128,
+            )
+            * 0.5,
+            torch.randn(
+                batch_size, dtype=torch.double if not cmplx else torch.complex128
+            ),
+        ]
+    )
+    a.requires_grad = a_requires_grad
+    x.requires_grad = x_requires_grad
+    zi.requires_grad = zi_requires_grad
+
+    assert gradcheck(LTIRecurrence.apply, (a, zi, x), check_forward_ad=True)
+    assert gradgradcheck(LTIRecurrence.apply, (a, zi, x), check_fwd_over_rev=True)
