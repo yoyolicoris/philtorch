27c5e0c7f3b216702694fa7426d11ec7c13b0143
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2026-03-15 15:50:02 +0000

    refactor: update cubic_spline test to include scipy_padding parameter

--- tests/test_interp.py
+++ tests/test_interp.py
@@ -21,17 +21,24 @@ from philtorch.lti.interp import cubic_spline, cspline
     ],
 )
 @pytest.mark.parametrize("pfe", [True, False])
-def test_cubic_spline(n, m, device, pfe):
+@pytest.mark.parametrize("scipy_padding", [True, False])
+def test_cubic_spline(n, m, device, pfe, scipy_padding):
     x = torch.randn(n, device=device).double()
-    x = torch.cat([x, x.flip(0)[1:]])  # make it periodic
+    if not scipy_padding:
+        x = torch.cat([x, x.flip(0)[1:]])  # make it periodic
 
-    y_philtorch = cubic_spline(x.unsqueeze(0), m, parallel_form=pfe).squeeze().cpu()
-
-    t = torch.arange(len(x)).double()
-    # cs = CubicSpline(t.cpu().numpy(), x.cpu().numpy(), bc_type="periodic")
+    y_philtorch = (
+        cubic_spline(x.unsqueeze(0), m, scipy_padding=scipy_padding, parallel_form=pfe)
+        .squeeze()
+        .cpu()
+    )
     t_new = torch.arange(0, len(x) - 1 + 1 / m / 2, 1 / m, dtype=torch.double)
-    # y_scipy = cs(t_new.cpu().numpy())
-    y_scipy = cspline1d_eval(cspline1d(x.cpu().numpy()), t_new.cpu().numpy())
+    if scipy_padding:
+        y_scipy = cspline1d_eval(cspline1d(x.cpu().numpy()), t_new.cpu().numpy())
+    else:
+        t = torch.arange(len(x)).double()
+        cs = CubicSpline(t.cpu().numpy(), x.cpu().numpy(), bc_type="periodic")
+        y_scipy = cs(t_new.cpu().numpy())
 
     assert torch.allclose(
         y_philtorch, torch.from_numpy(y_scipy)
@@ -53,14 +60,12 @@ def test_cubic_spline(n, m, device, pfe):
 )
 @pytest.mark.parametrize("pfe", [True, False])
 def test_cspline(n, device, pfe):
-    # x = torch.randn(n, device=device).double()
-    x = torch.tensor([0.0, 1.0] * (n // 2), device=device).double()  # make it periodic
-    y_philtorch = cspline(x.unsqueeze(0), parallel_form=pfe).squeeze().cpu()
+    x = torch.randn(n, device=device).double()
+    y_philtorch = (
+        cspline(x.unsqueeze(0), parallel_form=pfe, scipy_padding=True).squeeze().cpu()
+    )
     y_scipy = cspline1d(x.cpu().numpy())
 
-    print("Philtorch:", y_philtorch)
-    print("SciPy:", y_scipy)
-    print(x)
     assert torch.allclose(
         y_philtorch, torch.from_numpy(y_scipy)
     ), f"Max abs diff: {(y_philtorch - torch.from_numpy(y_scipy)).abs().max()}"
